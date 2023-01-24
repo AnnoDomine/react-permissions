8c1adcedc7b6091c51c3c669b88e0d02dcd4d9ef
AnnoDomine <AnnoDomine@users.noreply.github.com>
2023-01-24 21:31:27 +0100

    reassambling

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 export { default as PermissionProvider } from "./components/provider/PermissionProvider";
 export { default as Permission } from "./components/component/Permission";
 export { default as usePermission } from "./components/hooks/usePermission";
-
-function sayHiTo(name: string) {
-    return `Hi, ${name}`;
-}
-
-const message = sayHiTo("Bruno");
-
-console.log(message);
