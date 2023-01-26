746135790837741eeef727f50009c0140526d5e8
Dominic Seel <AnnoDomine@users.noreply.github.com>
2023-01-26 21:34:23 +0100

    Delete packages directory

--- packages/react-permission/lib/cjs/components/component/Permission.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { ComponentProps } from "../../types";
-declare const Permission: ({ permissions, noPermissionsNeeded, children, }: ComponentProps) => JSX.Element;
-export default Permission;
--- packages/react-permission/src/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./permission_component";
-export * from "./permission_provider";
-export * from "./permission_hook";
